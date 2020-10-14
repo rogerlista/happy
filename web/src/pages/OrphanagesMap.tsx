628515464466194f79a0a549185883aba526410f
Rogério Gomes Rios <roger.lista@gmail.com>
2020-10-14 13:00:29 -0300

    páginas de criar e exibir orfanato

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -50,14 +50,14 @@ function OrphanagesMap() {
             className="map-popup"
           >
             Lar das meninas
-            <Link to="">
+            <Link to="/orphanages/1">
               <FiArrowRight size={20} color="fff" />
             </Link>
           </Popup>
         </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
